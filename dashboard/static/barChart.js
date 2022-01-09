07abee48693169adc12e3c6a11a86ba79338d3d1
LeaHeinen <heinen.lea@gmail.com>
2022-01-09 16:41:04 +0100

    Add brushing functionality

--- dashboard/static/barChart.js
+++ dashboard/static/barChart.js
@@ -1,3 +1,10 @@
+// TODO:
+// Brushing
+// Annotations
+// Styling
+// Comments
+// Function of script
+
 const buttonMonth_div = document.getElementById("ButtonMonth");
 const buttonWeek_div = document.getElementById("ButtonWeek");
 const buttonDay_div = document.getElementById("ButtonDay");
@@ -10,6 +17,10 @@ var parseDateWeeks = d3.timeFormat("CW %U/%Y");
 var parseDateMonths = d3.timeFormat("%b %Y");
 
 function getTickArray(dataArray){
+		if (dataArray.length <= 5) {
+			console.log(dataArray.map(d => d.date))
+			return dataArray.map(d => d.date)
+		}
     const indicesBetweenPoints = Math.round(dataArray.length/12)
     tickArray = []
     for (i = 0; i <= dataArray.length; i++){
@@ -20,6 +31,22 @@ function getTickArray(dataArray){
     }
   }
   return tickArray.map(d => d.date)
+}
+
+function cutArray(array, start, end){
+	startIndex = 0;
+	endIndex = 0;
+	i = 0
+	iterator = array.values()
+	for (const value of iterator){
+		if (value["date"] == start){
+			startIndex = i
+		} if (value["date"] == end){
+			endIndex = i
+		}
+		i += 1
+	}
+	return array.slice(startIndex, endIndex+1)
 }
 
 // set svg dimensions and margins
@@ -36,14 +63,18 @@ let svg = d3.select("#barChart")
   .append("g")
       .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-function updateChart(){
-  console.log("Updating chart")
-}
-const render = data => {
-  dataArray = Array.from(data, ([date, value]) => ({ date, value }));
-//  monthArray = Array.from(months,  ([date, value]) => ({ date, value }));
+const render = (data, startDate, endDate) => {
+	svg.selectAll('*').remove();
+	if(data.constructor === Array){
+		data = dataArray
+		dataArray = cutArray(dataArray, startDate, endDate)
+		console.log(dataArray)
+	} else {
+		dataArray = Array.from(data, ([date, value]) => ({ date, value }));
+		originalData = dataArray.slice() //copies the array, so when used double clicks, this can be drawn
+	}
 
-  const xScale = d3.scaleBand()
+  xScale = d3.scaleBand()
     .domain(dataArray.map(d => d.date))
     .range([0, svgWidth])
 
@@ -51,10 +82,11 @@ const render = data => {
 
   const xAxis = d3.axisBottom(xScale)
       .tickValues(ticks)
+			.tickSizeOuter(0)
   const xAxisGroup = svg.append('g')
     .attr("transform", `translate(0, ${svgHeight})`)
     .call(xAxis)
-  xAxisGroup.selectAll('.domain').remove();
+  //xAxisGroup.selectAll('.domain').remove();
 
   xAxisOffset = 40
   svg.append('text')
@@ -97,8 +129,28 @@ const render = data => {
     .append("title")
       .text(d => "Time Interval: " + d.date + "\n" + "Steps: " + d.value)
 
-  console.log("The axes should have titles")
+
+    // Add brushing
+    brush = d3.brushX()
+      .extent([[0,0], [svgWidth,svgHeight]])
+      .on("end", updateChart)
+
+    const brusher = svg.append("g")
+      .call(brush)
+
 };
+
+const updateChart = event => {
+	setTimeout(() => {  console.log("Updating..."); }, 20000);
+  extent = event.selection
+	if (extent == null){
+		return
+	}
+  selected =  xScale.domain().filter(d =>
+             	(extent[0] - xScale.bandwidth() <= xScale(d))
+							&& (xScale(d) <= extent[1]));
+	render(dataArray, selected[0], selected.at(-1))
+}
 
 d3.csv(dataset).then(data =>{
     data.forEach(d => {
@@ -109,29 +161,25 @@ d3.csv(dataset).then(data =>{
       d.Weeks = parseDateWeeks(d.Time)
       d.Months = parseDateMonths(d.Time)
     });
-    nestedDataHours = d3.rollup(data,  v => d3.sum(v, d => d.Steps), d => d.Hours)
-    nestedDataDays = d3.rollup(data,  v => d3.sum(v, d => d.Steps), d => d.Days)
-    nestedDataWeeks = d3.rollup(data,  v => d3.sum(v, d => d.Steps), d => d.Weeks)
-    nestedDataMonths = d3.rollup(data,  v => d3.sum(v, d => d.Steps), d => d.Months)
+    nestedDataHours = d3.rollup(data, v => d3.sum(v,d => d.Steps), d => d.Hours)
+    nestedDataDays = d3.rollup(data, v => d3.sum(v,d => d.Steps), d => d.Days)
+    nestedDataWeeks = d3.rollup(data, v => d3.sum(v,d => d.Steps), d => d.Weeks)
+    nestedDataMonths = d3.rollup(data, v => d3.sum(v,d => d.Steps), d => d.Months)
 
     buttonHour_div.addEventListener("click", function(){
-        svg.selectAll('*').remove();
-        render(nestedDataHours)
+        render(nestedDataHours, nestedDataHours[0], nestedDataHours[-1])
     })
 
     buttonDay_div.addEventListener("click", function(){
-        svg.selectAll('*').remove();
-        render(nestedDataDays)
+        render(nestedDataDays, nestedDataDays[0], nestedDataDays[-1])
     })
 
     buttonWeek_div.addEventListener("click", function(){
-        svg.selectAll('*').remove();
-        render(nestedDataWeeks)
+        render(nestedDataWeeks, nestedDataWeeks[0], nestedDataWeeks[-1])
     })
 
     buttonMonth_div.addEventListener("click", function(){
-        svg.selectAll('*').remove();
-        render(nestedDataMonths)
+        render(nestedDataMonths, nestedDataMonths[0], nestedDataMonths[-1])
     })
 
   })
