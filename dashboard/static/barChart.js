18e9a9483b15abe4b3fe634f93f508eeb78db6c4
LeaHeinen <heinen.lea@gmail.com>
2022-01-09 11:26:49 +0100

    Add axes, labels and a tooltip>

--- dashboard/static/barChart.js
+++ dashboard/static/barChart.js
@@ -4,15 +4,28 @@ const buttonDay_div = document.getElementById("ButtonDay");
 const buttonHour_div = document.getElementById("ButtonHour");
 
 // Helping functions:
-var parseDateHours = d3.timeFormat("%d/%m/%Y %H");
+var parseDateHours = d3.timeFormat("%d/%m/%Y %H:00");
 var parseDateDays = d3.timeFormat("%d/%m/%Y");
-var parseDateWeeks = d3.timeFormat("%b/%Y %H");
-var parseDateMonths = d3.timeFormat("%m/%Y");
+var parseDateWeeks = d3.timeFormat("CW %U/%Y");
+var parseDateMonths = d3.timeFormat("%b %Y");
+
+function getTickArray(dataArray){
+    const indicesBetweenPoints = Math.round(dataArray.length/12)
+    tickArray = []
+    for (i = 0; i <= dataArray.length; i++){
+      if (i%indicesBetweenPoints == 0){
+        if (dataArray[i] != undefined){
+          tickArray.push(dataArray[i])
+        }
+    }
+  }
+  return tickArray.map(d => d.date)
+}
 
 // set svg dimensions and margins
-const svgWidth = 1400;
-const svgHeight = 800;
-const margin = {top: 110, right: 100, bottom: 150, left: 100};
+const svgWidth = 1200;
+const svgHeight = 600;
+const margin = {top: 10, right: 30, bottom: 50, left: 75}
 
 // append the svg object to the body of the page
 let svg = d3.select("#barChart")
@@ -20,28 +33,58 @@ let svg = d3.select("#barChart")
   .attr("width", svgWidth + margin.left + margin.right)
   .attr("height", svgHeight + margin.top + margin.bottom)
   .attr("class", "chart")
-const g =  svg.append("g")
+  .append("g")
+      .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-
+function updateChart(){
+  console.log("Updating chart")
+}
 const render = data => {
   dataArray = Array.from(data, ([date, value]) => ({ date, value }));
 //  monthArray = Array.from(months,  ([date, value]) => ({ date, value }));
 
-  const xScale = d3.scaleTime()
-    .domain(d3.extent(dataArray))
+  const xScale = d3.scaleBand()
+    .domain(dataArray.map(d => d.date))
     .range([0, svgWidth])
 
+  const ticks = getTickArray(dataArray)
 
   const xAxis = d3.axisBottom(xScale)
-      .ticks(12)
-  const xAxisGroup = g.append('g')
+      .tickValues(ticks)
+  const xAxisGroup = svg.append('g')
     .attr("transform", `translate(0, ${svgHeight})`)
     .call(xAxis)
+  xAxisGroup.selectAll('.domain').remove();
 
+  xAxisOffset = 40
+  svg.append('text')
+      .attr('class', 'axisLabel')
+      .attr('y', svgHeight + xAxisOffset)
+      .attr('x', svgWidth/2)
+      .attr('text-anchor', 'middle')
+      .text("Time Interval");
 
   const yScale = d3.scaleLinear()
     .domain([0, d3.max(dataArray, d => d.value)])
     .range([svgHeight, 0])
+
+  const yAxis = d3.axisLeft(yScale)
+    .tickSizeOuter(0)
+  const yAxisGroup = svg.append('g')
+    .call(yAxis)
+
+  yAxisOffset = -60
+  svg.append('text')
+    .attr('class', 'axisLabel')
+    .attr('y', yAxisOffset)
+    .attr('x', -svgHeight/2)
+    .attr('transform', `rotate(-90)`) // rotate axis by 90 degrees x->y and y->x
+    .attr('text-anchor', 'middle')
+    .text("Steps");
+
+  svg.selectAll(".tick")
+    .filter(function (d) { return d === 0;  })
+    .remove();
 
     svg.selectAll("rect")
     .data(dataArray)
@@ -50,7 +93,11 @@ const render = data => {
     .attr("x", d => xScale(d.date))
     .attr("y", d => yScale(d.value))
     .attr("height", d => svgHeight - yScale(d.value))
-    .attr("width", xScale.bandwidth());
+    .attr("width", xScale.bandwidth())
+    .append("title")
+      .text(d => "Time Interval: " + d.date + "\n" + "Steps: " + d.value)
+
+  console.log("The axes should have titles")
 };
 
 d3.csv(dataset).then(data =>{
