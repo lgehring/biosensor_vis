bc198eea54870bdc46eb226c51769025480d0542
nadnein <Nadjav.18@gmail.com>
2022-01-23 08:47:27 +0100

    finalized chordgraph (new colors, hover func., labels)

--- dashboard/static/chordgraph.js
+++ dashboard/static/chordgraph.js
@@ -1,4 +1,8 @@
-
+/***
+ * @author Nadja Volkmann 
+ ***/
+/*** Inspired by: https://www.d3-graph-gallery.com/graph/chord_axis_labels.html 
+ ***/
 
 
 function chordgraph(divName) {
@@ -27,14 +31,12 @@ function chordgraph(divName) {
             Temperature: 'interval',
             Steps: 'metric'
         };
-        console.log(cols)
 
         // create the correlation matrix for the variables: Calories, HR, Temperature, Steps
         var stats = new Statistics(data, columns);
         var corr_matrix = [...Array(cols.length - 1)].map(e => Array(cols.length - 1));
         for (let i=1; i < cols.length; i++){
             for (let j=i; j < cols.length; j++) {
-                console.log(cols[i], cols[j]);
                 var r = stats.correlationCoefficient(cols[i], cols[j]);
                 corr_matrix[i-1][j-1] = Math.abs(r.correlationCoefficient);
                 corr_matrix[j-1][i-1] = Math.abs(r.correlationCoefficient);
@@ -44,12 +46,12 @@ function chordgraph(divName) {
 
 
         // specify colors for each variable in chord graph 
-        var colors = [ "orange", "red", "pink", "purple"]
+        var colors = ["#00963c", "#e0432f", "#EB8015", "#245d96"]
         // create the svg area
         var svg = d3.select(divName)
             .append("svg")
-            .attr("width", 480)
-            .attr("height", 480)
+            .attr("width", 600)
+            .attr("height", 600)
             .append("g")
             .attr("transform", "translate(240,240)")
 
@@ -71,7 +73,11 @@ function chordgraph(divName) {
                 .radius(190)
             )
             .style("fill", function(d){ return(colors[d.source.index]) })
-            .style("stroke", "black");
+            .style("stroke", "black")
+            .style("opacity", 0.6)
+            .attr("class", function(d) { return "var " + cols[d.source.index +1] + " " + cols[d.target.index +1]})
+            .append("title")
+				.text(d => cols[d.source.index + 1] + " to " + cols[d.target.index + 1] + "\n" + "R: " + corr_matrix[d.source.index][d.target.index])
         
         // add the groups on the inner part of the circle
         var group = svg
@@ -92,18 +98,22 @@ function chordgraph(divName) {
         // Returns an array of tick angles and values for a given group and step.
         function groupTicks(d, step) {
             var k = (d.endAngle - d.startAngle) / d.value;
-            console.log(d3.range(0, d.value, step).map(function(value) {
-                return {value: value, angle: value * k + d.startAngle};
-                }))
-
             return d3.range(0, d.value, step).map(function(value) {
             return {value:  Math.round(value * 10) /10 , angle: value * k + d.startAngle};
             });
         }
 
+        // Return angle for label 
+        function groupLabel(d) {
+            console.log(d);
+            var k = (d.endAngle - d.startAngle) / d.value;
+            console.log({value:  Math.round(d.value/2 * 10) /10 , angle: d.value * k + d.startAngle, name: cols[d.index + 1]});
+            return [{value:  Math.round(d.value/2 * 10) /10 , angle: d.value/2 * k + d.startAngle, name: cols[d.index + 1]}];
+        }
+
         // Add the ticks
         group
-            .selectAll(".group-tick")
+            .selectAll(".cg-group-tick")
             .data(function(d) { return groupTicks(d, 0.2); })    // Controls the number of ticks: one tick each 25 here.
             .enter()
             .append("g")
@@ -111,14 +121,16 @@ function chordgraph(divName) {
             .append("line")               // By default, x1 = y1 = y2 = 0, so no need to specify it.
             .attr("x2", 10)
             .attr("stroke", "black")
+            
 
         // Add the labels of a few ticks:
         group
-            .selectAll(".group-tick-label")
+            .selectAll(".cg-group-tick-labels")
             .data(function(d) { return groupTicks(d, 0.2); })
             .enter()
             //.filter(function(d) { return d.value % 0.2 === 0; })
             .append("g")
+            .attr("class", "cg-tick-labels")
             .attr("transform", function(d) { return "rotate(" + (d.angle * 180 / Math.PI - 90) + ") translate(" + 205 + ",0)"; })
             .append("text")
             .attr("x", 8)
@@ -126,8 +138,37 @@ function chordgraph(divName) {
             .attr("transform", function(d) { return d.angle > Math.PI ? "rotate(180) translate(-16)" : null; })
             .style("text-anchor", function(d) { return d.angle > Math.PI ? "end" : null; })
             .text(function(d) { return d.value })
-            .style("font-size", 10)
-        
+            .style("font-size", 12)
 
+        
+        // highlights correlations for selected variable 
+        var highlightVariable = function(event, d) {
+            d3.selectAll(".var").style("opacity", 0.1)
+            d3.selectAll("." + cols[d.index + 1]).style("opacity", 1)
+        }
+
+        // highlights all correlations 
+        var highlightAll = function(event, d) {
+            d3.selectAll(".var").style("opacity", 0.6)
+        }
+
+        var names = ['Calories', 'Heartrate', 'Temperature', 'Steps']
+        // add variable names as titles
+        group.append("svg:text")
+            .each(function(d) { d.angle = (d.startAngle + d.endAngle) / 2; })
+            .attr("dy", ".35em")
+            .attr("class", "cg-titles")
+            .attr("text-anchor", function(d) { return d.angle > Math.PI ? "end" : null; })
+            .attr("transform", function(d) {
+		        return "rotate(" + (d.angle * 180 / Math.PI - 90) + ")"
+		            + "translate(" + (190 + 55) + ")"
+		            + (d.angle > Math.PI ? "rotate(180)" : "");
+                })
+            .attr('opacity', 1)
+            .text(function(d) { console.log(names[d.index]); return names[d.index]; })
+            .style("font-size", 20)
+            .on("mouseover", (event, d) => {highlightVariable(event, d)})
+            .on("mouseout", (event,d) => {highlightAll(event, d)})
+        
     })
 }
