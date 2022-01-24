63df95518a164ace1552569802b302b400944b15
L. Gehring <l.gehring@student.uni-tuebingen.de>
2022-01-24 19:05:30 +0100

    Cleaned up scatterplot.js and boxplot.js Updated the About.html to include information about the boxplot visualization

--- dashboard/static/boxplot.js
+++ dashboard/static/boxplot.js
@@ -3,9 +3,10 @@
  * based on: https://www.d3-graph-gallery.com/graph/boxplot_basic.html
  ***/
 
-// Constructs a boxplot in the given div.
-// Enter name of visparameter as given in the CSV Header and dotcolor as a string.
-function boxplot(visparameter, divName, dotcolor, start = null, end = null) {
+// Constructs a boxplot for the given parameter in the given div in the given color.
+// Enter name of visparameter as given in the CSV Header and boxcolor as a string.
+// Optionally takes a start and end date for the data used as datetime objects.
+function boxplot(visparameter, divName, boxcolor, start = null, end = null) {
     const margin = {top: 10, right: 30, bottom: 30, left: 60}, width = 200 - margin.left - margin.right,
         height = 600 - margin.top - margin.bottom;
 
@@ -23,16 +24,17 @@ function boxplot(visparameter, divName, dotcolor, start = null, end = null) {
         return roundedDate
     }
 
-    // Round start/end if available
+    // Round start/end date if available
     if (start != null && end != null) {
-        // round to nearest minute
+        // Round to nearest minute
         start = getRoundedDate(1, start)
         end = getRoundedDate(1, end)
     }
 
-    // Read data
+    // Read and process data
     d3.csv(dataset).then(function (data) {
-        // Preprocess --------------------------------------------------------------------------
+        // Preprocess data ----------------------------------------------------------
+        // Converts strings from the CSV to number or null
         function toNumber(d) {
             if (d != '') {
                 return +d
@@ -41,8 +43,8 @@ function boxplot(visparameter, divName, dotcolor, start = null, end = null) {
             }
         }
 
-        let visCol = []
-        // Read in as correct datatypes
+        let visparamData = []
+        // Read data and convert to correct type
         data.forEach(function (d) {
             d.Time = d3.isoParse(d.Time);
             d.Calories = toNumber(d.Calories);
@@ -51,75 +53,69 @@ function boxplot(visparameter, divName, dotcolor, start = null, end = null) {
             d.Steps = toNumber(d.Steps);
 
             if (start != null && end != null) {
-                if (start.valueOf() <= d.Time.valueOf() && d.Time.valueOf()<= end.valueOf()) {
-                    // only add data, when in timeframe
-                    visCol.push(eval("d." + visparameter))
+                // If start/end given, choose only values inside timeframe
+                if (start.valueOf() <= d.Time.valueOf() && d.Time.valueOf() <= end.valueOf()) {
+                    visparamData.push(eval("d." + visparameter))
                 }
             } else {
                 // No start/end defined, include all values
-                visCol.push(eval("d." + visparameter))
+                visparamData.push(eval("d." + visparameter))
             }
         });
         // --------------------------------------------------------------------------
-        // Compute summary statistics used for the box:
-        let data_sorted = visCol.sort(d3.ascending).filter(function(value, index, arr){
-            return value != 0;
-        });
-        //console.log(data_sorted)
-        let q1 = d3.quantile(data_sorted, .25)
-        //console.log(q1)
-        let median = d3.quantile(data_sorted, .5)
-        let q3 = d3.quantile(data_sorted, .75)
-        //console.log(q3)
-        let interQuantileRange = q3 - q1
-        //console.log(interQuantileRange)
-        let min = q1 - 1.5 * interQuantileRange
-        //console.log(min)
-        let max = q1 + 1.5 * interQuantileRange
-        //console.log(max)
 
-        // Show the Y scale
+        // Calculate boxplot parameters
+        let visparamData_sorted = visparamData.sort(d3.ascending)
+        let q1 = d3.quantile(visparamData_sorted, .25)
+        let median = d3.quantile(visparamData_sorted, .5)
+        let q3 = d3.quantile(visparamData_sorted, .75)
+        let interQuantileRange = q3 - q1
+        let min = q1 - 1.5 * interQuantileRange
+        let max = q1 + 1.5 * interQuantileRange
+
+        // Add Y scale
         let y = d3.scaleLinear()
-            .domain([min - interQuantileRange/4, max + interQuantileRange/4])
+            .domain([min - interQuantileRange / 4, max + interQuantileRange / 4])
             .range([height, 0]);
         svg.call(d3.axisLeft(y))
-        
-        // adds y-axis label - changed by Marit
-        // units
+
+        // Add Y-axis label - changed by Marit
+        // Units
         const unitHR = " [BPM]";
         const unitTemp = " [F]";
         const unitCal = " [1]";
         const unitSteps = " [1]";
 
-        // get unit -- implemented by Marit
-        function unit(param){
+        // Get Y-unit - implemented by Marit
+        function unit(param) {
             unit = "";
-            if(param == "HR"){
-            unit = unitHR;
-            } else if (param == "Temperature"){
-            unit = unitTemp;
-            } else if (param == "Calories"){
+            if (param == "HR") {
+                unit = unitHR;
+            } else if (param == "Temperature") {
+                unit = unitTemp;
+            } else if (param == "Calories") {
                 unit = unitCal;
-            } else if (param == "Steps"){
+            } else if (param == "Steps") {
                 unit = unitSteps;
             }
             return unit;
         }
 
+        // Add Y-unit
         svg.append("text")
             .attr("text-anchor", "end")
             .style("font-size", "14px")
             .style("font-family", "Arial")
             .attr("transform", "rotate(-90)")
-            .attr("y", -margin.left/2.8)
-            .attr("x", -margin.top )
+            .attr("y", -margin.left / 2.8)
+            .attr("x", -margin.top)
             .text(visparameter + unit(visparameter));
 
-        // a few features for the box
+        // Box location/size
         let center = 75
         let width = 75
 
-        // Show the main vertical line
+        // Show main vertical line
         svg
             .append("line")
             .attr("x1", center)
@@ -129,7 +125,7 @@ function boxplot(visparameter, divName, dotcolor, start = null, end = null) {
             .attr("stroke", "black")
             .style('stroke-width', 2)
 
-        // Show the box
+        // Show box
         svg
             .append("rect")
             .attr("x", center - width / 2)
@@ -138,9 +134,9 @@ function boxplot(visparameter, divName, dotcolor, start = null, end = null) {
             .attr("width", width)
             .attr("stroke", "black")
             .style('stroke-width', 2)
-            .style("fill", dotcolor)
+            .style("fill", boxcolor)
 
-        // show median, min and max horizontal lines
+        // Show median, min and max horizontal lines
         svg
             .selectAll("toto")
             .data([min, median, max])
